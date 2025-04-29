042f798da6193d5f44d3e6663b0c4f2b10129e55
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-29 10:54:08 -0300

    feat: add first example for hugging face

---
+++ 01_first.py
@@ -0,0 +1,13 @@
+from torch.fx.proxy import Scope
+from transformers import pipeline
+
+modelo = pipeline("fill-mask")
+frase = "The capital of <mask> is Brasilia."
+predicoes = modelo(frase)
+
+for predicao in predicoes:
+    resposta = predicao["token_str"]
+    score = predicao["score"]
+    frase = predicao["sequence"]
+    score_ajustado = score * 100
+    print(f"Predição {resposta} com {score_ajustado:.2f}% de certeza -> {frase}")
