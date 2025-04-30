04760d8d84ecf5e5aab1917c7f89787a3fcbbfc7
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-30 12:04:36 -0300

    feat: add new chat bot

--- 03_chatbot.py
+++ 03_chatbot.py
@@ -1,6 +1,22 @@
+import warnings
+warnings.filterwarnings("ignore", message="To copy construct from a tensor*")
 from transformers import pipeline
 
-chatbot = pipeline('text-generation', model='Felladrin/Llama-68M-Chat-v1', max_new_tokens=256)
-pergunta = "Hi, what is your name?"
-resposta = chatbot(pergunta)
-print(resposta)
+chatbot = pipeline('text-generation', 
+model='Felladrin/Llama-68M-Chat-v1', max_new_tokens=256, penalty_alpha=0.6, top_k=4)
+
+
+system_message = "You are a helpful assistant."
+chat = system_message
+
+while True:
+    user_message = input("Usuário: ")
+    if user_message == "sair":
+        break
+    print('User:', user_message)
+    chat += f"<|im_start|>user\n{user_message}<|im_end|>\n"
+    output = chatbot(chat)
+    chat = output[0]['generated_text'].split('<|im_start|>assistant\n')[-1].rstrip('<|im_end|>')
+    formatted_chat = chat.split('<|im_start|>assistant\n')[-1].rstrip('<|im_end|>')
+    print('Assistant:', formatted_chat)
+
